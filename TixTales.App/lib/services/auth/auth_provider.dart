3fbd4c497ff798a09291531dabe22c17aac24168
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-02-13 13:44:08 +0300

    Documented auth provide calss

--- TixTales.App/lib/services/auth/auth_provider.dart
+++ TixTales.App/lib/services/auth/auth_provider.dart
@@ -1,17 +1,29 @@
 import 'dart:ffi';
 
+import 'auth_user.dart';
+
 //"Interface" for auth provider
 abstract class AuthProvider {
+  //Initialize you service
+  Future<void> initialize();
+
+  //get current user
+  AuthUser get getCurrentUser;
+
   Future<Void> login({
     required email,
     required password,
   });
 
+  //create user
   Future<Void> createUser({
     required email,
     required password,
   });
+
+  //logout user
   Future<Void> logout();
 
+  //send email verification
   Future<Void> sendEmailVerification();
 }
