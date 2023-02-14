304adac6f8841993f05e6b401888a1c08db6a3fa
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-02-14 10:21:03 +0300

    Create and implement auth service

---
+++ TixTales.App/lib/services/auth/auth_service.dart
@@ -0,0 +1,43 @@
+import 'dart:ffi';
+
+import 'package:tix_tales/services/auth/auth_provider.dart';
+import 'package:tix_tales/services/auth/auth_user.dart';
+import 'package:tix_tales/services/auth/firebase/firebase_auth_provider.dart';
+
+class AuthService extends AuthProvider {
+  final AuthProvider provider;
+
+  AuthService(this.provider);
+
+  //create auth provider based on firebase auth provider
+  factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) =>
+      provider.createUser(email: email, password: password);
+
+  @override
+  AuthUser? get currentUser => provider.currentUser;
+
+  @override
+  Future<void> initialize() => provider.initialize();
+
+  @override
+  Future<Void> logOut() => provider.logOut();
+
+  @override
+  Future<AuthUser> login({
+    required String email,
+    required String password,
+  }) =>
+      provider.login(
+        email: email,
+        password: password,
+      );
+
+  @override
+  Future<Void> sendEmailVerification() => provider.sendEmailVerification();
+}
