a62f12c3619daeb8f345694c310c269fbdab4e60
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-02-15 12:15:03 +0300

    Created home page

---
+++ TixTales.App/lib/views/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tix_tales/services/auth/bloc/auth_bloc.dart';
+import 'package:tix_tales/services/auth/bloc/auth_event.dart';
+import 'package:tix_tales/src/Constants/app_resources.dart';
+import 'package:tix_tales/src/Constants/routes.dart';
+import 'package:tix_tales/views/onboarding_view.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+              'Welocome to the home page',
+              style: AppResources.appStyles.textStyles.bodyDefault.copyWith(
+                color: AppResources.appColors.globalDark,
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                context.read<AuthBloc>().add(
+                      const AuthEventLogout(),
+                    );
+                Navigator.pushNamedAndRemoveUntil(
+                    context, onboardingRoute, (route) => false);
+              },
+              child: const Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
