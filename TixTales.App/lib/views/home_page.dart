2c1b4ab95a441aede298dfa86244697b2da0364f
Enoch Mwangi <enoch.mwangi@outlook.com>
2023-02-19 22:18:11 +0300

    Code refactoring

--- TixTales.App/lib/views/home_page.dart
+++ TixTales.App/lib/views/home_page.dart
@@ -1,36 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:tix_tales/services/auth/bloc/auth_bloc.dart';
-import 'package:tix_tales/services/auth/bloc/auth_event.dart';
+import 'package:tix_tales/src/Constants/all_constant_imports.dart';
 import 'package:tix_tales/src/Constants/app_resources.dart';
-import 'package:tix_tales/src/Constants/routes.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? selectedValue;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text(
-              'Welocome to the home page',
-              style: AppResources.appStyles.textStyles.bodyDefault.copyWith(
-                color: AppResources.appColors.globalDark,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(36.5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Find evnts in',
+                style: AppResources.appStyles.textStyles.bodySmall.copyWith(
+                  color: AppResources.appColors.typographyGrey,
+                ),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                context.read<AuthBloc>().add(
-                      const AuthEventLogout(),
-                    );
-                Navigator.pushNamedAndRemoveUntil(
-                    context, onboardingRoute, (route) => false);
-              },
-              child: const Text('Logout'),
-            ),
-          ],
+              const SizedBox(height: 8.5),
+              SizedBox(
+                height: 20,
+                child: Row(children: [
+                  Image.asset(AppAssets.mapPinIcon),
+                  const SizedBox(width: 5),
+                  DropdownButton<String>(
+                    underline: Container(),
+                    style: AppResources.appStyles.textStyles.headineH6.copyWith(
+                      color: AppResources.appColors.typographyDark,
+                      fontSize: 18,
+                    ),
+                    value: selectedValue,
+                    hint: const Text('Select an item'),
+                    items: const [
+                      DropdownMenuItem(
+                        value: 'Barelona',
+                        child: Text('Barcelona'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'Nairobi',
+                        child: Text('Nairobi'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'Mombasa',
+                        child: Text('Mombasa'),
+                      ),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        selectedValue = value;
+                      });
+                    },
+                  ),
+                ]),
+              ),
+              const SizedBox(height: 30.5),
+              Text('Popular in $selectedValue'),
+            ],
+          ),
         ),
       ),
     );
