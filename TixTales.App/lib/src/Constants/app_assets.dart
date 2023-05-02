49f6e4d8f309e18ffb9d58299f2cdeaaf93fc62c
IamEnoch <enoch.mwangi@outlook.com>
2023-05-02 11:24:34 +0300

    Definitiion for ticket image

--- TixTales.App/lib/src/Constants/app_assets.dart
+++ TixTales.App/lib/src/Constants/app_assets.dart
@@ -19,4 +19,5 @@ class AppAssets {
   static const String ticketIcon = "assets/images/ticketIcon.png";
   static const String userIcon = "assets/images/userIcon.png";
   static const String testImage = "assets/images/testImage.png";
+  static const String greenTicketIcon = "assets/images/greenTicket.png";
 }
