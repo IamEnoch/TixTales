26afc1d314684976264b853e9f20558093349b52
IamEnoch <enoch.mwangi@outlook.com>
2023-05-05 01:57:20 +0300

    Created a new model

---
+++ TixTales.App/lib/services/events/ticket_details.dart
@@ -0,0 +1,13 @@
+class TicketDetails {
+  final String? eventName;
+  final String? eventDate;
+  final String? ticketsBought;
+  final String? thumbnail;
+
+  TicketDetails({
+    this.eventName,
+    this.eventDate,
+    this.ticketsBought,
+    this.thumbnail,
+  });
+}
