0fa7df5847fe92b0b5d0c8e13a52f69004f3ff09
IamEnoch <enoch.mwangi@outlook.com>
2023-05-04 16:55:31 +0300

    Update UI of the upcming tab

--- TixTales.App/lib/widgets/ticket_card.dart
+++ TixTales.App/lib/widgets/ticket_card.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -16,39 +18,55 @@ class _TicketCardState extends State<TicketCard> {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(0),
-      clipBehavior: Clip.antiAlias,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Row(
         children: [
-          SizedBox(width: MediaQuery.of(context).size.width * 0.026),
-          Image.asset(
-            AppAssets.greenTicketIcon,
-            width: 31,
-            height: 31,
-          ),
-          SizedBox(width: MediaQuery.of(context).size.width * 0.047),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.350,
+          Padding(
+            padding: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width * 0.026),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('La rosalia',
-                    style: AppResources.appStyles.textStyles.bodyDefaultBold),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.005,
+                Row(
+                  children: [
+                    //SizedBox(width: MediaQuery.of(context).size.width * 0.026),
+                    Image.asset(
+                      AppAssets.greenTicketIcon,
+                      width: 31,
+                      height: 31,
+                    ),
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.037),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.40,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('La rosalia',
+                              style: AppResources
+                                  .appStyles.textStyles.bodyDefaultBold),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.005,
+                          ),
+                          Text('Date',
+                              style:
+                                  AppResources.appStyles.textStyles.bodySmall),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Text('Date',
+                SizedBox(height: MediaQuery.of(context).size.height * 0.015),
+                Text('2 tickets',
                     style: AppResources.appStyles.textStyles.bodySmall),
               ],
             ),
           ),
-          SizedBox(width: MediaQuery.of(context).size.width * 0.0867),
           Expanded(
             child: Ink.image(
               image: const NetworkImage(
                   "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"),
               width: 10,
-              height: 96,
+              height: MediaQuery.of(context).size.height * 0.125,
               fit: BoxFit.cover,
             ),
           ),
